44d2b71d2a357a19afffe69848861aa3cd1c34ec
Marcello Pontes <balaum@gmail.com>
2015-06-03 20:58:27 +0200

    id #10157
    There’s a new functionality here.
    
    Users will be able to inject css and js on mantle[PUC]. To do so,
    they’ll need to create into their template folder, a json file named
    template-config.json and add to it an array called “externalResources”
    of elements of the type:
    {
     type : [String | “css”,”js”]
     context : [String | “mantle” ] this can be used later for more contexts
     url : [String | the url relative to the template folder]
    }
    
    Unfortunately, when these resources are loaded, there’s only jquery
    1.4.4 available, so the 404 handling is… nonexistent.
    
    I’ve added to tapa-default template, an example that places a logotype
    at pucHeader.

---
+++ resources/templates/external-resources.js
@@ -0,0 +1,63 @@
+/*
+	This is the only file included at plugin.xml
+	as a js reference to mantle context.
+*/
+
+(function(){
+
+	var templatesPath = "content/tapa/resources/templates";
+	var jsPath = "";
+
+	/* 	@marpontes - only jquery 1.4.4 available, so.. old style.
+
+		Reads config.json to determine the current template name
+		and loads template-config.json within to inject the 
+		"externalResources" when on mantle context.
+	*/
+	$.getJSON( templatesPath+"/config.json",  function(config){
+
+		var template = config.currentTemplate;
+		var templatePath = templatesPath + "/" + template;
+		jsPath =  templatePath + "/template-config.json";
+
+		console.log("[TapaPlugin] Current Template is: "+template);
+		console.log("[TapaPlugin] Loading "+ jsPath);
+
+		$.getJSON(jsPath,function(pconf){
+
+			if(pconf===null || undefined === pconf.externalResources){
+				console.log("[TapaPlugin] No externalResources at "+ jsPath);
+				return;
+			}
+
+			var er = pconf.externalResources;
+
+			for(index in er){
+				var url = er[index].url;
+				var type = er[index].type;
+
+				appendResource(templatePath+"/"+url,type);
+			}
+		});
+
+	});
+
+	/*	appends css or js to the <head> tag.
+	*/
+	function appendResource(url,type){
+		if(type==="css"){
+			$("<link rel='stylesheet' type='text/css' href='"+ url +"?context=mantle'/>")
+			.appendTo("head");
+		}else if(type==="js"){
+			$("<script type='text/javascript' src='"+ url +"'></scr"+"ipt>")
+			.appendTo("head");
+		}else{
+			console.log("[TapaPlugin] There might be something wrong with your template config. "+
+				"But no harm done");
+		}
+		
+	}
+
+
+})();
+
---
+++ resources/templates/tapa-default/assets/js/mantle-context.js
@@ -0,0 +1,3 @@
+// Appends a div to PUC header where we'll place our logo
+
+$("#pucHeader").append("<div id=\"tapaLogo\"></div>");
