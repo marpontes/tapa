96dbe5420a3cb0da3725eead2f494c1bc2c86873
Marcello Pontes <balaum@gmail.com>
2015-10-09 00:05:51 +0200

    Fixes after Pentaho 6 testing

--- resources/templates/external-resources.js
+++ resources/templates/external-resources.js
@@ -15,42 +15,57 @@
 	var templatesPath = CONTEXT_PATH+"content/tapa/resources/templates";
 	var jsPath = "";
 
-	/* 	@marpontes - only jquery 1.4.4 available, so.. old style.
+	if(typeof $ === "undefined"){
+		require(['cdf/lib/jquery'],function($){
+			loadTapaResources($);
+		});
+	}else{
+		loadTapaResources($);
+	}
 
-		Reads config.json to determine the current template name
-		and loads template-config.json within to inject the 
-		"externalResources" when on mantle context.
-	*/
-	$.getJSON( CONTEXT_PATH+"plugin/tapa/api/getconfig",  function(config){
-		
-		if (config["pluginEnabled"] !== true)
-			return;
 
-		var template = config.currentTemplate;
-		var templatePath = templatesPath + "/" + template;
-		jsPath =  templatePath + "/template-config.json";
+	function loadTapaResources($){
 
-		console.log("[TapaPlugin] Current Template is: "+template);
-		console.log("[TapaPlugin] Loading "+ jsPath);
 
-		$.getJSON(jsPath,function(pconf){
+		/* 	@marpontes - only jquery 1.4.4 available, so.. old style.
 
-			if(pconf===null || undefined === pconf.externalResources){
-				console.log("[TapaPlugin] No externalResources at "+ jsPath);
+			Reads config.json to determine the current template name
+			and loads template-config.json within to inject the 
+			"externalResources" when on mantle context.
+		*/
+		$.getJSON( CONTEXT_PATH+"plugin/tapa/api/getconfig",  function(config){
+			
+			if (config["pluginEnabled"] !== true)
 				return;
-			}
 
-			var er = pconf.externalResources;
+			var template = config.currentTemplate;
+			var templatePath = templatesPath + "/" + template;
+			jsPath =  templatePath + "/template-config.json";
 
-			for(index in er){
-				var url = er[index].url;
-				var type = er[index].type;
+			console.log("[TapaPlugin] Current Template is: "+template);
+			console.log("[TapaPlugin] Loading "+ jsPath);
 
-				appendResource(templatePath+"/"+url,type);
-			}
+			$.getJSON(jsPath,function(pconf){
+
+				if(pconf===null || undefined === pconf.externalResources){
+					console.log("[TapaPlugin] No externalResources at "+ jsPath);
+					return;
+				}
+
+				var er = pconf.externalResources;
+
+				for(index in er){
+					var url = er[index].url;
+					var type = er[index].type;
+
+					appendResource(templatePath+"/"+url,type);
+				}
+			});
+
 		});
 
-	});
+	} 
+
 
 	/*	appends css or js to the <head> tag.
 	*/
